f278a6d2921ae1fb710dc9250db78a00da9da0bb
NAIM2046 <nmd98911@gmail.com>
2025-02-01 17:24:27 +0600

    home page demo complate

--- Frontend/src/Components/Navber/Footer/Footer.jsx
+++ Frontend/src/Components/Navber/Footer/Footer.jsx
@@ -4,46 +4,66 @@ import youtub from "./../../../../public/assets/youTub.png";
 import facebook from "./../../../../public/assets/sm-icons-facebook-logo.webp";
 import instgram from "./../../../../public/assets/instagram.png";
 import linkeden from "./../../../../public/assets/linkeden.png";
+import { SlLocationPin } from "react-icons/sl";
+import { FaPhoneVolume } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-gray-300 py-10 px-5">
-      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-10">
+    <footer className="bg-base-300 text-base-content  py-10 px-5">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 text-center md:text-left">
         {/* Logo & Info */}
-        <div className="flex flex-col items-center lg:items-start text-center lg:text-left space-y-3">
+        <div className="flex flex-col items-center md:items-start space-y-3">
           <img className="w-auto max-w-48" src={logo} alt="RDF Logo" />
           <p className="text-sm text-gray-400">
-            ACME Industries Ltd. <br />
-            Providing reliable tech since 1992
+            Resource Development Foundation(RDF) <br />
+            Providing reliable Service since 1992
           </p>
         </div>
 
-        {/* Links */}
-        <div className="lg:flex lg:flex-wrap justify-center lg:justify-start gap-12 text-center text-xl space-y-4">
-          <div>
-            <h6 className="font-semibold text-white mb-3 text-2xl border-b-2">
-              Find Us
-            </h6>
-            <a className="block hover:text-blue-400 transition">Branding</a>
-            <a className="block hover:text-blue-400 transition">Design</a>
-            <a className="block hover:text-blue-400 transition">Marketing</a>
-            <a className="block hover:text-blue-400 transition">
-              Advertisement
-            </a>
+        {/* Find Us */}
+        <div>
+          <h6 className="font-semibold  mb-3 text-xl border-b-2 pb-2">
+            Find Us
+          </h6>
+          <div className="space-y-3">
+            <p className="flex gap-1">
+              <FaMapMarkerAlt className=" text-2xl" />{" "}
+              <span className="text-xl font-semibold">Address:</span>
+            </p>
+            <p className="flex items-center gap-2">
+              Head Office: RDF BHABAN, House #21, Road #12, Pisciculture Housing
+              Society, Block-Kha, Adabor, Dhaka-1207
+            </p>
+            <p className="flex items-center gap-2">
+              <FaPhoneVolume /> +8802-22243850, +8801733065522
+            </p>
+            <p className="flex items-center gap-2">
+              <MdEmail /> info@rdfbd.org
+            </p>
           </div>
-          <div>
-            <h6 className="font-semibold text-white mb-3 text-2xl border-b-2">
-              About Us
-            </h6>
+        </div>
+
+        {/* About Us */}
+        <div>
+          <h6 className="font-semibold  mb-3 text-xl border-b-2 pb-2">
+            About Us
+          </h6>
+          <div className="space-y-2">
             <a className="block hover:text-blue-400 transition">Our Story</a>
             <a className="block hover:text-blue-400 transition">Contact</a>
             <a className="block hover:text-blue-400 transition">Careers</a>
             <a className="block hover:text-blue-400 transition">Press</a>
           </div>
-          <div>
-            <h6 className="font-semibold text-white mb-3 text-2xl border-b-2">
-              Useful Links
-            </h6>
+        </div>
+
+        {/* Useful Links */}
+        <div>
+          <h6 className="font-semibold  mb-3 text-xl border-b-2 pb-2">
+            Useful Links
+          </h6>
+          <div className="space-y-2">
             <a className="block hover:text-blue-400 transition">Terms of Use</a>
             <a className="block hover:text-blue-400 transition">
               Privacy Policy
@@ -53,28 +73,28 @@ const Footer = () => {
             </a>
           </div>
         </div>
+      </div>
 
-        {/* Newsletter */}
-        <div className="text-center lg:text-left">
-          <h6 className="font-semibold text-white mb-3 text-2xl">
-            Subscribe to Our Newsletter
-          </h6>
-          <div className="flex flex-col sm:flex-row gap-3">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="px-4 py-2 bg-gray-800 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition cursor-pointer">
-              Subscribe
-            </button>
-          </div>
+      {/* Newsletter */}
+      <div className="mt-10 text-center">
+        <h6 className="font-semibold mb-3 text-xl">
+          Subscribe to Our Newsletter
+        </h6>
+        <div className="flex flex-col sm:flex-row gap-3 justify-center">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="px-4 py-2  rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition cursor-pointer">
+            Subscribe
+          </button>
         </div>
       </div>
 
       {/* Social & Copyright */}
       <div className="mt-10 border-t border-gray-700 pt-5 text-center">
-        <p className="text-gray-400 mb-4 text-2xl font-semibold">
+        <p className="text-gray-400 mb-4 text-xl font-semibold">
           Follow us on social media
         </p>
         <div className="flex justify-center gap-5 mb-4">
@@ -92,8 +112,7 @@ const Footer = () => {
           </a>
         </div>
         <p className="text-sm text-gray-500">
-          &copy; {new Date().getFullYear()} ACME Industries Ltd. All rights
-          reserved.
+          &copy; {new Date().getFullYear()} RDF BD. All rights reserved.
         </p>
       </div>
     </footer>
