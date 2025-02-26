10889c9dea7fa8981957143136a1fdd7c2cf4db3
NAIM2046 <nmd98911@gmail.com>
2025-02-26 13:32:14 +0600

    add server side

--- Frontend/src/Hook/useAxiosPublice.jsx
+++ Frontend/src/Hook/useAxiosPublice.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 const AxiosPublic = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://rdfwebsite-production.up.railway.app",
 });
 const useAxiosPublic = () => {
   return AxiosPublic;
--- Frontend/src/storage/useRDFstorage.jsx
+++ Frontend/src/storage/useRDFstorage.jsx
@@ -9,7 +9,9 @@ const useRDFStore = create((set) => ({
   fetchsliderinfo: async () => {
     set({ isLoading: true });
     try {
-      const response = await axios.get("http://localhost:3001/slider");
+      const response = await axios.get(
+        "https://rdfwebsite-production.up.railway.app/slider"
+      );
       set({ sliderinfo: response.data, isLoading: false });
     } catch (error) {
       console.error("Error fetching projects:", error);
